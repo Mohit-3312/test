8b3fde83f945f5b77220f66e73ee5b7be4c265b6
Mohit-3312 <bhutiyamohit6@gmail.com>
2025-09-29 13:43:28 +0530

    First Commit

---
+++ hello-servlet/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!doctype html><html><head><title>Hello</title></head><body>");
+            out.println("<h1>Hello, World! 👋</h1>");
+            out.println("</body></html>");
+        }
+    }
+}
+
+
